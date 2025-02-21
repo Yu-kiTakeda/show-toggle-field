1b9c53a27e528a74609b8ce98e9e502a78699980
yamada <yamada@fistbump.biz>
2025-02-21 17:02:27 +0900

    事件管理　記録アプリ初期設定修正

--- src/js/config/initalData.js
+++ src/js/config/initalData.js
@@ -34,6 +34,229 @@ export async function initalData(appId) {
                     }
                     ]
                 },
+                {
+                    applyPage: 'all',
+                    isShowDefault: false,
+                    selectFields: ['顧問'],
+                    conditions: [
+                    {
+                        compare: "equal",
+                        compareVal: "顧問",
+                        field: "case_types_category",
+                        isOr: true,
+                    }
+                    ]
+                },
+                {
+                    applyPage: 'all',
+                    isShowDefault: false,
+                    selectFields: ['顧問'],
+                    conditions: [
+                    {
+                        compare: "equal",
+                        compareVal: "顧問",
+                        field: "case_types_category",
+                        isOr: true,
+                    }
+                    ]
+                },
+                {
+                    applyPage: 'all',
+                    isShowDefault: false,
+                    selectFields: ['被相続人'],
+                    conditions: [
+                    {
+                        compare: "equal",
+                        compareVal: "遺言，相続",
+                        field: "case_types_category",
+                        isOr: true,
+                    }
+                    ]
+                },
+                {
+                    applyPage: 'all',
+                    isShowDefault: false,
+                    selectFields: ['被相続人'],
+                    conditions: [
+                    {
+                        compare: "equal",
+                        compareVal: "遺言，相続",
+                        field: "case_types_category",
+                        isOr: true,
+                    }
+                    ]
+                },
+                {
+                    applyPage: 'all',
+                    isShowDefault: false,
+                    selectFields: ['月々の金額'],
+                    conditions: [
+                    {
+                        compare: "equal",
+                        compareVal: "月々の金額",
+                        field: "分割方法",
+                        isOr: true,
+                    }
+                    ]
+                },
+                {
+                    applyPage: 'all',
+                    isShowDefault: false,
+                    selectFields: ['分割回数'],
+                    conditions: [
+                    {
+                        compare: "equal",
+                        compareVal: "回数",
+                        field: "分割方法",
+                        isOr: true,
+                    }
+                    ]
+                },
+                {
+                    applyPage: 'all',
+                    isShowDefault: false,
+                    selectFields: ['タイムチャージ一覧','HiddenGroup'],
+                    conditions: [
+                    {
+                        compare: "equal",
+                        compareVal: "",
+                        field: "",
+                        isOr: true,
+                    }
+                    ]
+                },
+            ];
+        }
+        if(appInfo.name == '記録') {
+            data = [
+                {
+                    applyPage: 'all',
+                    isShowDefault: true,
+                    selectFields: ['重要度', 'カテゴリ','発信者','折り返し電話番号','ConfirmedUser','対応チェック','通知先'],
+                    conditions: [
+                    {
+                        compare: "equal",
+                        compareVal: "受電",
+                        field: "formType",
+                        isOr: true,
+                    }
+                    ]
+                },
+                {
+                    applyPage: 'all',
+                    isShowDefault: false,
+                    selectFields: ['scheduleType'],
+                    conditions: [
+                    {
+                        compare: "equal",
+                        compareVal: "スケジュール",
+                        field: "formType",
+                        isOr: true,
+                    }
+                    ]
+                },
+                {
+                    applyPage: 'all',
+                    isShowDefault: true,
+                    selectFields: ['日時'],
+                    conditions: [
+                    {
+                        compare: "equal",
+                        compareVal: "スケジュール",
+                        field: "formType",
+                        isOr: true,
+                    },
+                    {
+                        compare: "equal",
+                        compareVal: "ToDo",
+                        field: "formType",
+                        isOr: true,
+                    }
+                    ]
+                },
+                {
+                    applyPage: 'all',
+                    isShowDefault: false,
+                    selectFields: ['締切日','事件種類','関連TODO一覧','関連タスクID','タスク名','状態','担当者'],
+                    conditions: [
+                    {
+                        compare: "equal",
+                        compareVal: "ToDo",
+                        field: "formType",
+                        isOr: true,
+                    }
+                    ]
+                },
+                {
+                    applyPage: 'all',
+                    isShowDefault: false,
+                    selectFields: ['公開非公開','alertDay'],
+                    conditions: [
+                    {
+                        compare: "equal",
+                        compareVal: "スケジュール",
+                        field: "formType",
+                        isOr: true,
+                    }
+                    ]
+                },
+                {
+                    applyPage: 'all',
+                    isShowDefault: true,
+                    selectFields: ['startDate','endDate','allDay','equipment','繰り返し予定'],
+                    conditions: [
+                    {
+                        compare: "equal",
+                        compareVal: "受電",
+                        field: "formType",
+                        isOr: true,
+                    },
+                    {
+                        compare: "equal",
+                        compareVal: "経過記録",
+                        field: "formType",
+                        isOr: true,
+                    },
+                    {
+                        compare: "equal",
+                        compareVal: "ToDo",
+                        field: "formType",
+                        isOr: true,
+                    },
+                    ]
+                },
+                {
+                    applyPage: 'all',
+                    isShowDefault: true,
+                    selectFields: ['scheduleTitle','公開非公開'],
+                    conditions: [
+                    {
+                        compare: "equal",
+                        compareVal: "受電",
+                        field: "formType",
+                        isOr: true,
+                    },
+                    {
+                        compare: "equal",
+                        compareVal: "経過記録",
+                        field: "formType",
+                        isOr: true,
+                    },
+                    ]
+                },
+                {
+                    applyPage: 'all',
+                    isShowDefault: false,
+                    selectFields: ['HiddenGroup'],
+                    conditions: [
+                    {
+                        compare: "equal",
+                        compareVal: "",
+                        field: "",
+                        isOr: true,
+                    }
+                    ]
+                },
             ];
         }
 
